2219b1ee7889d58aa6257120d705df9ae6e4889c
isafecode <soft.work.inbox@gmail.com>
2020-07-18 18:14:36 +0530

    first commit

---
+++ foo.js
@@ -0,0 +1,4 @@
+function getOk()
+{
+  alert('Hi');
+}
---
+++ fooController.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="app.js" />
+
+app.controller("fooController", function ($scope) {
+
+    model = $scope;
+
+    model.init = function () {
+        alert('You clicked me :)');
+    };
+
+    model.YourName = "Pavan";
+
+
+    model.Users = [
+        {
+            "userid": 1,
+            "name": "Ram"
+        },
+        {
+            "userid": 2,
+            "name": "Hari"
+        }, {
+
+            "userid": 3,
+            "name": "Krishna"
+        }];
+
+    model.SelectedUser = $scope.Users[3];
+});
